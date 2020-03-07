552f183ffc1280a727359ba30a79ed0571b28e22
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-03-07 13:19:57 -0700

    updated movie descriptions in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@ Movie.create(
 title: 'The Dark Night',
 duration: '2hr 32min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0468569/',
+description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+# 'http://www.imdb.com/title/tt0468569/',
 trailer: 'https://www.youtube.com/embed/EXeTwQWrcwY')
 
 
@@ -18,221 +19,285 @@ Movie.create(
 title: 'The Bourne Identity',
 duration: '1hr 59min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0258463/',
+description: 'A man is picked up by a fishing boat, bullet-riddled and suffering from amnesia, before racing to elude assassins and attempting to regain his memory.',
+# 'http://www.imdb.com/title/tt0258463/',
 trailer: 'https://www.youtube.com/embed/cD-uQreIwEk'
 )
+
 Movie.create(
 title: 'Avatar',
 duration: '2hr 42min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0499549/?ref_=fn_al_tt_1',
+description: 'A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.',
+# 'http://www.imdb.com/title/tt0499549/?ref_=fn_al_tt_1',
 trailer: 'https://www.youtube.com/embed/5PSNL1qE6VY'
 )
+
 Movie.create(
 title: 'Kill Bill Vol 1',
 duration: '1hr 51min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0266697/?ref_=nv_sr_1',
+description: 'After awakening from a four-year coma, a former assassin wreaks vengeance on the team of assassins who betrayed her.',
+# 'http://www.imdb.com/title/tt0266697/?ref_=nv_sr_1',
+'After awakening from a four-year coma, a former assassin wreaks vengeance on the team of assassins who betrayed her.',
 trailer: 'https://www.youtube.com/embed/ot6C1ZKyiME')
+
 Movie.create(
 title: 'Gladiator',
 duration: '2hr 35min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0172495/?ref_=nv_sr_1',
+description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
+# 'http://www.imdb.com/title/tt0172495/?ref_=nv_sr_1',
 trailer: 'https://www.youtube.com/embed/owK1qxDselE'
 )
+
 Movie.create(
 title: 'The Rock',
 duration: '2hr 16min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0117500/',
+description: 'A mild-mannered chemist and an ex-con must lead the counterstrike when a rogue group of military men, led by a renegade general, threaten a nerve gas attack from Alcatraz against San Francisco.',
+# 'http://www.imdb.com/title/tt0117500/',
 trailer: 'https://www.youtube.com/embed/313n0wga2xo'
 )
+
 Movie.create(
 title: 'Jaws',
 duration: '2hr 4min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0073195/',
+description: "When a killer shark unleashes chaos on a beach community, it's up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down.",
+# 'http://www.imdb.com/title/tt0073195/',
 trailer: 'https://www.youtube.com/embed/U1fu_sA7XhE'
 )
+
 Movie.create(
 title: 'Star Wars Episode IV A New Hope',
 duration: '2hr 1min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0076759/',
+description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
+# 'http://www.imdb.com/title/tt0076759/',
 trailer: 'https://www.youtube.com/embed/1g3_CFmnU7k'
 )
+
 Movie.create(
 title: 'Star Wars Episode V The Empire Strikes Back',
 duration: '2hr 4min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0080684/',
+description: "After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued by Darth Vader and a bounty hunter named Boba Fett all over the galaxy.",
+# 'http://www.imdb.com/title/tt0080684/',
 trailer: 'https://www.youtube.com/embed/mz_YWNhKOkM'
 )
+
 Movie.create(
 title: 'Star Wars Episode VI Return of the Jedi',
 duration: '2hr 11min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0086190/',
+description: "After a daring mission to rescue Han Solo from Jabba the Hutt, the Rebels dispatch to Endor to destroy the second Death Star. Meanwhile, Luke struggles to help Darth Vader back from the dark side without falling into the Emperor's trap.",
+# 'http://www.imdb.com/title/tt0086190/',
 trailer: 'https://www.youtube.com/embed/7L8p7_SLzvU'
 )
+
 Movie.create(
 title: 'Batman Begins',
 duration: '2hr 20min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0372784/',
+description: "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from corruption.",
+# 'http://www.imdb.com/title/tt0372784/',
 trailer: 'https://www.youtube.com/embed/QhPqez3CwiM'
 )
+
 Movie.create(
 title: 'Top Gun',
 duration: '1hr 50min',
 genre: 'Action',
-description: 'http://www.imdb.com/title/tt0092099/',
+description: "As students at the United States Navy's elite fighter weapons school compete to be best in the class, one daring young pilot learns a few things from a civilian instructor that are not taught in the classroom.",
+# 'http://www.imdb.com/title/tt0092099/',
 trailer: 'https://www.youtube.com/embed/qAfbp3YX9F0'
 )
+
 Movie.create(
 title: 'Dogma',
 duration: '2hr 10min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0120655/',
+description: "An abortion clinic worker with a special heritage is called upon to save the existence of humanity from being negated by two renegade angels trying to exploit a loop-hole and reenter Heaven.",
+# 'http://www.imdb.com/title/tt0120655/',
 trailer: 'https://www.youtube.com/embed/20CRw3XdETA'
 )
+
 Movie.create(
-title: 'Team America World Peace',
+title: 'Team America World Police',
 duration: '1hr 38min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0372588/',
+description: "Popular Broadway actor Gary Johnston is recruited by the elite counter-terrorism organization Team America: World Police. As the world begins to crumble around him, he must battle with terrorists, celebrities and falling in love.",
+# 'http://www.imdb.com/title/tt0372588/',
 trailer: 'https://www.youtube.com/embed/RPBX47zSktc'
 )
+
 Movie.create(
 title: 'Zoolander',
 duration: '1hr 30min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0196229/?ref_=nv_sr_2',
+description: "At the end of his career, a clueless fashion model is brainwashed to kill the Prime Minister of Malaysia.",
+# 'http://www.imdb.com/title/tt0196229/?ref_=nv_sr_2',
 trailer: 'https://www.youtube.com/embed/YtQq0T3ExLs'
 )
+
 Movie.create(
 title: 'Superbad',
 duration: '1hr 53min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0829482/?ref_=nv_sr_1',
+description: "Two co-dependent high school seniors are forced to deal with separation anxiety after their plan to stage a booze-soaked party goes awry.",
+# 'http://www.imdb.com/title/tt0829482/?ref_=nv_sr_1',
 trailer: 'https://www.youtube.com/embed/MNpoTxeydiY'
 )
+
 Movie.create(
 title: 'The Hangover',
 duration: '1hr 40min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt1119646/?ref_=nv_sr_1',
+description: "Three buddies wake up from a bachelor party in Las Vegas, with no memory of the previous night and the bachelor missing. They make their way around the city in order to find their friend before his wedding.",
+# 'http://www.imdb.com/title/tt1119646/?ref_=nv_sr_1',
 trailer: 'https://www.youtube.com/embed/vhFVZsk3XEs'
 )
+
 Movie.create(
 title: 'Weird Science',
 duration: '1hr 34min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0090305/',
+description: "Two high school nerds use a computer program to literally create the perfect woman, but she turns their lives upside down.",
+# 'http://www.imdb.com/title/tt0090305/',
 trailer: 'https://www.youtube.com/embed/9qd04u2Yj44'
 )
+
 Movie.create(
 title: 'Major League',
 duration: '1hr 47min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0097815/',
+description: "The new owner of the Cleveland Indians puts together a purposely horrible team so they'll lose and she can move the team. But when the plot is uncovered, they start winning just to spite her.",
+# 'http://www.imdb.com/title/tt0097815/',
 trailer: 'https://www.youtube.com/embed/7O6QHXQE6Cc'
 )
+
 Movie.create(
 title: 'The Internship',
 duration: '1hr 59min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt2234155/',
+description: "Two salesmen whose careers have been torpedoed by the digital age find their way into a coveted internship at Google, where they must compete with a group of young, tech-savvy geniuses for a shot at employment.",
+# 'http://www.imdb.com/title/tt2234155/',
 trailer: 'https://www.youtube.com/embed/cdnoqCViqUo'
 )
+
 Movie.create(
 title: 'Back to the Future',
 duration: '1hr 56min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0088763/',
+description: "Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the eccentric scientist Doc Brown.",
+# 'http://www.imdb.com/title/tt0088763/',
 trailer: 'https://www.youtube.com/embed/qvsgGtivCgs'
 )
+
 Movie.create(
 title: 'Anchorman',
 duration: '1hr 34min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0357413/',
+description: "Ron Burgundy is San Diego's top-rated newsman in the male-dominated broadcasting of the 1970s, but that's all about to change for Ron and his cronies when an ambitious woman is hired as a new anchor.",
+# 'http://www.imdb.com/title/tt0357413/',
 trailer: 'https://www.youtube.com/embed/NJQ4qEWm9lU'
 )
+
 Movie.create(
 title: 'Wedding Crashers',
 duration: '1hr 59min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0396269/',
+description: "John Beckwith and Jeremy Grey, a pair of committed womanizers who sneak into weddings to take advantage of the romantic tinge in the air, find themselves at odds with one another when John meets and falls for Claire Cleary.",
+# 'http://www.imdb.com/title/tt0396269/',
 trailer: 'https://www.youtube.com/embed/omI8i1a7rlQ'
 )
+
 Movie.create(
 title: 'Dazed and Confused',
 duration: '1hr 42min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0106677/',
+description: "The adventures of high school and junior high students on the last day of school in May 1976.",
+# 'http://www.imdb.com/title/tt0106677/',
 trailer: 'https://www.youtube.com/embed/3aQuvPlcB-8'
 )
+
 Movie.create(
 title: "Ferris Beuller's Day Off",
 duration: '1hr 43min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0091042/',
+description: "A high school wise guy is determined to have a day off from school, despite what the Principal thinks of that.",
+# 'http://www.imdb.com/title/tt0091042/',
 trailer: 'https://www.youtube.com/embed/R-P6p86px6U'
 )
+
 Movie.create(
 title: "Monty Python's Holy Grail",
 duration: '1hr 31min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0071853/',
+description: "King Arthur and his Knights of the Round Table embark on a surreal, low-budget search for the Holy Grail, encountering many, very silly obstacles.",
+# 'http://www.imdb.com/title/tt0071853/',
 trailer: 'https://www.youtube.com/embed/LG1PlkURjxE'
 )
+
 Movie.create(
 title: 'Blues Brothers',
 duration: '2hr 13min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0080455/',
+description: "Jake Blues, just released from prison, puts together his old band to save the Catholic home where he and his brother Elwood were raised.",
+# 'http://www.imdb.com/title/tt0080455/',
 trailer: 'https://www.youtube.com/embed/A-xtJYIwfYo'
 )
+
 Movie.create(
 title: 'Animal House',
 duration: '1hr 49min',
 genre: 'Comedy',
-description: 'http://www.imdb.com/title/tt0077975/',
+description: "At a 1962 college, Dean Vernon Wormer is determined to expel the entire Delta Tau Chi Fraternity, but those troublemakers have other plans for him.",
+# 'http://www.imdb.com/title/tt0077975/',
 trailer: 'https://www.youtube.com/embed/BoS3-yHoaSY'
 )
+
 Movie.create(
 title: 'The Godfather',
 duration: '2hr 55min',
 genre: 'Drama',
-description: 'http://www.imdb.com/title/tt0068646/',
+description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+'http://www.imdb.com/title/tt0068646/',
 trailer: 'https://www.youtube.com/embed/5DO-nDW43Ik'
 )
+
 Movie.create(
 title: 'Rocky',
 duration: '2hr 00min',
 genre: 'Drama',
-description: 'http://www.imdb.com/title/tt0075148/',
+description: "A small-time boxer gets a supremely rare chance to fight a heavy-weight champion in a bout in which he strives to go the distance for his self-respect.",
+# 'http://www.imdb.com/title/tt0075148/',
 trailer: 'https://www.youtube.com/embed/3VUblDwa648'
 )
+
 Movie.create(
 title: 'Forrest Gump',
 duration: '2hr 22min',
 genre: 'Drama',
-description: 'http://www.imdb.com/title/tt0109830/',
+description: "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+# 'http://www.imdb.com/title/tt0109830/',
 trailer: 'https://www.youtube.com/embed/YNh9Es8Ut6U'
 )
+
 Movie.create(
 title: 'Pulp Fiction',
 duration: '2hr 34min',
 genre: 'Drama',
-description: 'http://www.imdb.com/title/tt0110912/',
+description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+# 'http://www.imdb.com/title/tt0110912/',
 trailer: 'https://www.youtube.com/embed/s7EdQ4FqbhY'
 )
+
 Movie.create(
 title: 'Swingers',
 duration: '1hr 36min',
 genre: 'Drama',
-description: 'http://www.imdb.com/title/tt0117802/',
+description: "Wannabe actors become regulars in the stylish neo-lounge scene; Trent teaches his friend Mike the unwritten rules of the scene.",
+# 'http://www.imdb.com/title/tt0117802/',
 trailer: 'https://www.youtube.com/embed/nWCct8XbQD0)
